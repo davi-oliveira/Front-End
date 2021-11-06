132b8889af209fbff18153f970a6f16a50b5dbd1
Davi Oliveira <davirdoliveira@gmail.com>
2021-11-06 15:38:29 -0300

    alterações

--- 10) atividade-condicionais/condicional02/script.js
+++ 10) atividade-condicionais/condicional02/script.js
@@ -2,18 +2,36 @@ var frmMinutos
 var divMostrar
 var valor
 var teste
+var teste2
 var diferenca
 
 btnCalc.addEventListener('click', function(){
     frmMinutos = minutos.value
-    divMostrar = document.getElementById('mostrarValor')
+    
+    if(frmMinutos <= 0 || frmMinutos % 1 !== 0) { return alert('Digite uma quantidade válida de minutos') }
 
-    if(frmMinutos <= 0) { return alert('Digite uma quantidade válida de minutos') }
-    teste = frmMinutos/15
     diferenca = frmMinutos%15
-    valor = 5*teste
-    if(diferenca >= 1) { valor = parseInt(valor)+5}
-    if(frmMinutos < 15) { valor = 5 }
+    if(diferenca == 0 && valor != 5)
+    {
+        teste = frmMinutos/15
+        valor = teste*5
+    }
+    else if(diferenca > 0)
+    {
+        teste2 = frmMinutos - diferenca
+        console.log(teste2)
+        teste2 = teste2+15
+        teste = teste2/15
+        valor = teste*5
+    }
+
+    if(frmMinutos < 15)
+    {
+        valor = 5
+    }
+
+    // Mostrar
+    divMostrar = document.getElementById('mostrarValor')
     divMostrar.innerHTML = `
         <h4>
             <p>Valor a ser pago pelo cliente em ${frmMinutos} minutos: R$ ${valor}</p>
