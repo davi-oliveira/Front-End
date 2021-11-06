1163db8fb00a23e0d418a93109d2e2deaef2eb58
Davi Oliveira <davirdoliveira@gmail.com>
2021-11-06 15:50:02 -0300

    terminado

--- 10) atividade-condicionais/condicional02/script.js
+++ 10) atividade-condicionais/condicional02/script.js
@@ -4,37 +4,38 @@ var valor
 var teste
 var teste2
 var diferenca
+var preco
 
 btnCalc.addEventListener('click', function(){
     frmMinutos = minutos.value
+    preco = prcPorMinuto.value
     
     if(frmMinutos <= 0 || frmMinutos % 1 !== 0) { return alert('Digite uma quantidade válida de minutos') }
 
     diferenca = frmMinutos%15
-    if(diferenca == 0 && valor != 5)
+    if(diferenca == 0 && valor != preco)
     {
         teste = frmMinutos/15
-        valor = teste*5
+        valor = teste*preco
     }
     else if(diferenca > 0)
     {
         teste2 = frmMinutos - diferenca
-        console.log(teste2)
         teste2 = teste2+15
         teste = teste2/15
-        valor = teste*5
+        valor = teste*preco
     }
 
     if(frmMinutos < 15)
     {
-        valor = 5
+        valor = preco
     }
 
     // Mostrar
     divMostrar = document.getElementById('mostrarValor')
     divMostrar.innerHTML = `
         <h4>
-            <p>Valor a ser pago pelo cliente em ${frmMinutos} minutos: R$ ${valor}</p>
+            <br><p>Valor a ser pago pelo cliente em ${frmMinutos} minutos: R$ ${valor}</p>
         </h4>
     `
 })
