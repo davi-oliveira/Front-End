31929c5e1348e17aa70de0cfaf1e18f6d79782e8
Davi Oliveira <davirdoliveira@gmail.com>
2021-12-12 14:52:37 -0300

    atts

--- #exemplos/nomes_cidades/script.js
+++ #exemplos/nomes_cidades/script.js
@@ -1,4 +1,4 @@
-let result = fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados/GO/distritos')
+let result = fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados/GO/distritos?orderBy=nome')
 let container = document.querySelector('#editTxt')
 
 result.then(r => {
@@ -7,6 +7,7 @@ result.then(r => {
   j.forEach(element => {
     container.innerHTML += `
       <tr>
+        <td>${element.id}</td>
         <td>GO</td>
         <td>${element.nome}</td>
       </tr>
