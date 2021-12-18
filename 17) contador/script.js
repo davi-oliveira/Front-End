bd88565ef066a47bcbeaba9e063431a9f4273786
Davi Oliveira <davirdoliveira@gmail.com>
2021-12-18 20:57:05 -0300

    teste

--- 17) contador/script.js
+++ 17) contador/script.js
@@ -1,7 +1,16 @@
-let frmData = document.getElementById('data')
-
-btnCalc.addEventListener("click", cronometrar(frmData.value))
-
-function cronometrar(data){
+btnCont.addEventListener('click', function(){
+    if(document.getElementById('cmp-data').value == ""){
+        return alert('Selecione uma data primeiro!')
+    }
     
-}
+    let anoSelec = document.getElementById('cmp-data').value.substr(0, 4)
+    let mesSelec = document.getElementById('cmp-data').value.substr(5, 2) 
+    let diaSelec = document.getElementById('cmp-data').value.substr(8, 3)
+    
+    
+    now = new Date
+    // now.getDate() //dia do mês
+    // now.getMounth() //mês
+    // now.getFullYear() //ano
+  
+})
