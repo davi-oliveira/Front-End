da293446d2921b718b17c09ded29cf91945270d0
Davi Oliveira <davirdoliveira@gmail.com>
2021-12-21 22:12:51 -0300

    finalizado com sucesso

--- 16) conselhos/script.js
+++ 16) conselhos/script.js
@@ -3,6 +3,7 @@ document.getElementById('paraConselho').style.display = 'none'
 document.getElementById('editTxt').style.display = 'none'
 document.getElementById('Autor').style.display = 'none'
 document.getElementById('apontando').style.display = 'none'
+document.getElementById('outro').style.display = 'none'
 
 var msg = ''
 
@@ -15,31 +16,30 @@ btnCons.addEventListener('click', function(){
     document.getElementById('editTxt').style.display = 'block'
     document.getElementById('Autor').style.display = 'block'
     document.getElementById('apontando').style.display = 'block'
+    document.getElementById('outro').style.display = 'block'
 
+    pegarImagem()
+})
+
+function pegarImagem(){
     fetch('https://api.adviceslip.com/advice')
-  .then(response => response.json())
+  .then(response => response.json(), document.getElementById('editTxt').innerHTML = 
+  `<progress class="progress is-small is-primary" max="100">15%</progress>`)
+
   .then(json => { 
-      msg = json.slip.advice
-      if(msg.length > 50) { reject() }
+        msg = json.slip.advice
+        if(msg.length > 55) { reject() }
+
       document.getElementById('editTxt').innerHTML = `${json.slip.advice}`
     })
+
    .catch(err => {
-        fetch('https://api.adviceslip.com/advice')
-        .then(response => response.json())
-        .then(json => { 
-            msg = json.slip.advice
-            if(msg.length > 50) { reject() }
-            document.getElementById('editTxt').innerHTML = `${json.slip.advice}`
-        })
-        .catch(err => {
-            fetch('https://api.adviceslip.com/advice')
-            .then(response => response.json())
-            .then(json => { 
-                msg = json.slip.advice
-                if(msg.length > 50) { reject() }
-                document.getElementById('editTxt').innerHTML = `${json.slip.advice}`
-            })
-       })
+        pegarImagem()
    })
-    
+}
+
+outro.addEventListener('click', function(){
+    pegarImagem()
+    let randNum = parseInt(Math.random() * (300 - 200) + 200)
+    document.getElementById('paraConselho').innerHTML = `<img src="https://picsum.photos/${randNum}/300" alt="" class="cabrinha" id="imgAleatoria">`
 })
